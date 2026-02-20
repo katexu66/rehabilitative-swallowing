d6277f46e9c30d619969c97daa13e0d066da0b1d
katexu66 <katexu66@gmail.com>
2026-02-20 14:28:14 -0500

    Adding grid lines/standardizing y-axis scaling

--- Rehabilitative swallowing website/launcher.py
+++ Rehabilitative swallowing website/launcher.py
@@ -6,7 +6,7 @@ import webbrowser
 def main():
     # start the server in the background
     p = subprocess.Popen([sys.executable, "-m", "uvicorn",
-                          "server_dummy:app",
+                          "server:app",
                           "--host", "127.0.0.1", "--port", "8000"],
                          stdout=subprocess.DEVNULL,
                          stderr=subprocess.DEVNULL)
